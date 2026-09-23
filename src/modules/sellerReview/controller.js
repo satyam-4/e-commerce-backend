84c86034b32b2ce19c9521400f4e9c1ac0b2d7f3
Satyam Mishra <prosatyam2003@gmail.com>
2026-09-23 10:00:41 +0000

    refactor(sellerReview): simplify createOrUpdateSellerReview by removing error handling

--- src/modules/sellerReview/controller.js
+++ src/modules/sellerReview/controller.js
@@ -1,49 +1,26 @@
 import { AppError } from "#utils/AppError.js";
-import { upsertSellerReview, upsertSellerReviewSummary } from "./repository.js";
-import { sellerVariantRepository } from "../sellers/repository.js";
+import { upsertSellerReviewAndSummary } from "./repository.js";
+import { findSellerByUserId } from "../sellers/repository.js";
 
 const createOrUpdateSellerReview = async (req, res) => {
-    try {
-        const { sellerId, rating, review } = req.body;
-        const user = req.user;
+    const { sellerId, rating, review } = req.body;
+    const { id: userId } = req.user;
 
-        const seller = await sellerVariantRepository.findSellerByUserId(user.id);
+    const seller = await findSellerByUserId(userId);
 
-        if (seller && (seller?.id === sellerId)) {
-            throw new AppError(403, "You cannot review yourself");
-        }
-
-        const { result, stats, breakdown } = await upsertSellerReview(user.id, sellerId, rate, review);
-        let initialBreakdown = {
-            "1": 0,
-            "2": 0,
-            "3": 0,
-            "4": 0,
-            "5": 0,
-        };
-
-        const ratingBreakdown = breakdown.reduce((acc, item) => {
-            const rate = Math.round(item.rating);
-            if (rating >= 0 && rating <= 5) acc[rate] += item._count;
-            return acc;
-        }, initialBreakdown);
-
-        await upsertSellerReviewSummary(sellerId, stats, ratingBreakdown);
-
-        return res
-        .status(200)
-        .json({
-            success: true,
-            message: "Successfully upserted seller review",
-            data: result
-        });
-    } catch (error) {
-        console.log("Error:", error);
-        if (error instanceof AppError) {
-            throw error;
-        }
-        throw new AppError(500, "Internal server error");
+    if (seller && (seller?.id === sellerId)) {
+        throw new AppError(403, "You cannot review yourself");
     }
+
+    const result = await upsertSellerReviewAndSummary(userId, sellerId, rating, review);
+
+    return res
+    .status(200)
+    .json({
+        success: true,
+        message: "Successfully upserted seller review",
+        data: result
+    });
 };
 
 export { createOrUpdateSellerReview };
